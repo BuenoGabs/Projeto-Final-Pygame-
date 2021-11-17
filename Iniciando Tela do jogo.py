7917cbb62c7a1eb5d997af31ba96cb0883964966
gabriel <gabrielb6@al.insper.edu.br>
2021-11-17 07:59:08 -0300

    Atualizando a tela do jogo

--- Iniciando Tela do jogo.py
+++ Iniciando Tela do jogo.py
@@ -6,11 +6,15 @@ pygame.init()
 
 # ----- Gera tela principal
 window = pygame.display.set_mode((500, 400))
-pygame.display.set_caption('Arma')
+pygame.display.set_caption('Gun')
 
 # ----- Inicia estruturas de dados
 game = True
 
+# ----- Inicia assets
+font = pygame.font.SysFont(None, 48) #fonte de texto
+text = font.render('Atire pra matar', True, (255, 0, 0)) #mensagem #deixar o texto mais leve #cor
+text_1 = font.render('Pressione Qualquer tecla', True, (0,0,255)) #mensagem #deixa o testo mais leve #cor
 # ===== Loop principal =====
 while game:
     # ----- Trata eventos
@@ -18,14 +22,15 @@ while game:
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
-        if event.type == pygame.KEYUP: #aqui o jogo fecha assim que qualquer tecla for apertada
-            game = False 
 
     # ----- Gera saídas
-    window.fill((0, 0, 0))  # Preenche com a cor preta
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(text, (10, 10)) #passando a posição do texto
+    window.blit(text_1, (40, 100)) #passando a posição do texto
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
