d42b0766b9edf974196164a8eca300295559d98f
gabriel <gabrielb6@al.insper.edu.br>
2021-11-19 15:34:15 -0300

    Cenário, movimento e sprites

---
+++ Controlando nosso protagonista.py
@@ -0,0 +1,124 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+
+pygame.init()
+
+# ----- Gera tela principal
+WIDTH = 800
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Shotto')
+
+# ----- Inicia assets
+Inimigo_WIDTH = 20
+Inimigo_HEIGHT = 20
+protag_WHIDTH = 80
+protag_HEIGHT = 80
+font = pygame.font.SysFont(None, 70)
+background = pygame.image.load('space.png').convert()  #carrega as imagens de fundo 
+enemy_img = pygame.image.load('ghost2.jpg').convert_alpha() #carrega as imagens do inimigo
+inimigo_img_small = pygame.transform.scale(enemy_img, (Inimigo_WIDTH, Inimigo_HEIGHT))
+protag_img = pygame.image.load('rambo.png').convert_alpha()
+protag_img = pygame.transform.scale(protag_img, (protag_WHIDTH, protag_HEIGHT))
+
+class Protag(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+    
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+class Enemy(pygame.sprite.Sprite):
+    def  __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, WIDTH-Inimigo_WIDTH)
+        self.rect.y = random.randint(-100, -Inimigo_HEIGHT)
+        self.speedx = random.randint(-3, 3)
+        self.speedy = random.randint(2, 9)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy 
+
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = random.randint(0, WIDTH-Inimigo_WIDTH)
+            self.rect.y = random.randint(-100, -Inimigo_HEIGHT)
+            self.speedx = random.randint(-3, 3)
+            self.speedy = random.randint(2, 9)
+
+# ----- Inicia estruturas de dados
+game = True
+clock = pygame.time.Clock()
+FPS = 30
+all_sprites = pygame.sprite.Group()
+# Criando o jogador
+player = Protag(protag_img)
+all_sprites.add(player)
+# Criando os meteoros
+for i in range(8):
+    Inimigo = Enemy(enemy_img)
+    all_sprites.add(Inimigo)
+
+# ===== Loop principal =====
+while game:
+    clock.tick(FPS)
+
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+    #verifica se apertou alguma tecla
+         # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+        # Verifica se apertou alguma tecla.
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx += 8
+        # Verifica se soltou alguma tecla.
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx -= 8
+    
+    # ----- Atualiza estado do jogo
+    # Atualizando a posição dos meteoros
+    all_sprites.update()
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(background, (0, 0))
+    # Desenhando meteoros
+    all_sprites.draw(window)
+
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
--- Trazendo nosso protagonista.py
+++ Trazendo nosso protagonista.py
@@ -1,6 +1,5 @@
 # ===== Inicialização =====
 # ----- Importa e inicia pacotes
-from os import kill
 import pygame
 import random
 
@@ -14,11 +13,37 @@ pygame.display.set_caption('Shotto')
 
 # ----- Inicia assets
 Inimigo_WIDTH = 20
-Inimigo_HEIGHT = 20
+Inimigo_HEIGHT = 10
+protag_WHIDTH = 80
+protag_HEIGHT = 80
 font = pygame.font.SysFont(None, 70)
 background = pygame.image.load('starfield.png').convert()  #carrega as imagens de fundo 
 enemy_img = pygame.image.load('ghost2.jpg').convert_alpha() #carrega as imagens do inimigo
 inimigo_img_small = pygame.transform.scale(enemy_img, (Inimigo_WIDTH, Inimigo_HEIGHT))
+protag_img = pygame.image.load('rambo.png').convert_alpha()
+protag_img = pygame.transform.scale(protag_img, (protag_WHIDTH, protag_HEIGHT))
+
+class Protag(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+    
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
 
 class Enemy(pygame.sprite.Sprite):
     def  __init__(self, img):
@@ -46,13 +71,19 @@ class Enemy(pygame.sprite.Sprite):
 game = True
 clock = pygame.time.Clock()
 FPS = 30
-
-Inimigo1 = Enemy(enemy_img)
-Inimigo2 = Enemy(enemy_img)
+all_sprites = pygame.sprite.Group()
+# Criando o jogador
+player =    Protag(protag_img)
+all_sprites.add(player)
+# Criando os meteoros
+for i in range(8):
+    Inimigo = Enemy(enemy_img)
+    all_sprites.add(Inimigo)
 
 # ===== Loop principal =====
 while game:
     clock.tick(FPS)
+
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
@@ -60,17 +91,17 @@ while game:
             game = False
 
     # ----- Atualiza estado do jogo
-    # Atualizando a posição do meteoro
-    Inimigo1.update()
-    Inimigo2.update()
+    # Atualizando a posição dos meteoros
+    all_sprites.update()
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(background, (0, 0))  # desenha a imagem de fundo e depois a imagem do meteoro
-    window.blit(Inimigo1.image, Inimigo1.rect)
-    window.blit(Inimigo2.image, Inimigo2.rect) # desenha a imagem de fundo e depois a imagem do inimigo
-    
+    window.blit(background, (0, 0))
+    # Desenhando meteoros
+    all_sprites.draw(window)
+
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
