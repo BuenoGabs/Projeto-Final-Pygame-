ca506d05aa7ad4b2f6db261a604691374ba08a65
gabriel <gabrielb6@al.insper.edu.br>
2021-11-22 21:07:36 -0300

    Sons no Game e atualização da mecânica geral

--- Iterando o jogo.py
+++ Iterando o jogo.py
@@ -2,12 +2,14 @@
 # ----- Importa e inicia pacotes
 import pygame
 import random
+import time 
 
 pygame.init()
+pygame.mixer.init()
 
 # ----- Gera tela principal
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1400
+HEIGHT = 750
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Shotto')
 
@@ -26,9 +28,16 @@ protag_img_mirror = pygame.transform.flip(protag_img, True, False)
 bullet_img = pygame.image.load('imagens Pygame/laserRed16.png').convert_alpha()
 bullet_img_mirror = pygame.transform.flip(bullet_img, True, False)
 
+# Carrega os sons do jogo
+pygame.mixer.music.load('Backgroundmusic.mp3')
+pygame.mixer.music.set_volume(0.4)
+boom_sound = pygame.mixer.Sound('expl3.wav')
+destroy_sound = pygame.mixer.Sound('expl6.wav')
+pew_sound = pygame.mixer.Sound('pew.wav')
+
 
 class Protag(pygame.sprite.Sprite):
-    def __init__(self, img, all_sprites, all_bullets, bullet_img):
+    def __init__(self, img, all_sprites, all_bullets, bullet_img, pew_sound):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
 
@@ -41,27 +50,14 @@ class Protag(pygame.sprite.Sprite):
         self.all_sprites = all_sprites
         self.all_bullets = all_bullets
         self.bullet_img = bullet_img
+        self.pew_sound =pew_sound
+
     
     def update(self):
         # Atualização da posição da protagonista
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 
-        # Mantem dentro da tela
-        
-        # if self.rect.right > WIDTH:
-        #     self.rect.right = WIDTH
-            
-            
-        # if self.rect.left < 0:
-        #     self.rect.left = 0
-
-        # if self.rect.bottom > HEIGHT:
-        #     self.rect.bottom = HEIGHT
-        
-        # if self.rect.top < 0:
-        #     self.rect.top = 0
-        
         #Contorno periodico
         if self.rect.right > WIDTH:
             self.rect.left = 0
@@ -75,13 +71,15 @@ class Protag(pygame.sprite.Sprite):
         
         if self.rect.top < 0:
             self.rect.bottom = HEIGHT
+        
+      
     
     def shoot(self):
         pos_bullet = self.rect.top + 50
-        print(pos_bullet)
         new_bullet = Bullet(self.bullet_img, pos_bullet, self.rect.centerx)
         self.all_sprites.add(new_bullet)
         self.all_bullets.add(new_bullet)
+        self.pew_sound.play()
 
         
 
@@ -120,12 +118,13 @@ class Bullet(pygame.sprite.Sprite):
         # Coloca no lugar inicial definido em x, y do constutor
         self.rect.centerx = centerx
         self.rect.bottom = bottom
-        self.speedy = 10  # Velocidade fixa para o lado
+        self.speedy = 100 # Velocidade fixa para o lado
+
 
     def update(self):
-        # A bala só se move no eixo y
+        # A bala só se move no eixo x
         self.rect.x += self.speedy
-        
+         
 
         # Se o tiro passar do inicio da tela, morre.
         if self.rect.bottom < 0:
@@ -141,8 +140,9 @@ all_sprites = pygame.sprite.Group()
 all_enemy = pygame.sprite.Group()
 all_bullets = pygame.sprite.Group()
 # Criando o jogador
-player = Protag(protag_img, all_sprites, all_bullets, bullet_img)
+player = Protag(protag_img, all_sprites, all_bullets, bullet_img, pew_sound)
 all_sprites.add(player)
+bullet = Bullet(bullet_img, HEIGHT - 10, WIDTH / 2)  
 # Criando os inimigos
 for i in range(8):
     Inimigo = Enemy(enemy_img)
@@ -150,9 +150,12 @@ for i in range(8):
     all_enemy.add(Inimigo)
 
 # ===== Loop principal =====
+pygame.mixer.music.play(loops=-1)
 while game:
     clock.tick(FPS)
 
+    direita = True
+    esquerda = True
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
@@ -162,20 +165,25 @@ while game:
         if event.type == pygame.KEYDOWN:
             # Dependendo da tecla, altera a velocidade.
             if event.key == pygame.K_LEFT:
+
                 player.image = protag_img_mirror
                 bullet_img = bullet_img_mirror
                 player.speedx -= 8
+                    
             if event.key == pygame.K_RIGHT:
                 player.speedx += 8
                 player.image = protag_img
                 bullet_img = bullet_img_mirror
+        
             if event.key == pygame.K_UP:
                 player.speedy -= 8
             if event.key == pygame.K_DOWN:
                 player.speedy += 8
             if event.key == pygame.K_SPACE:
+                
                 player.shoot()
                 bullet_img = bullet_img_mirror
+        
         # Verifica se soltou alguma tecla.
         if event.type == pygame.KEYUP:
             # Dependendo da tecla, altera a velocidade.
@@ -194,12 +202,23 @@ while game:
 
     all_sprites.update()
 
+    hits = pygame.sprite.groupcollide(all_enemy, all_bullets, True, True)
+    for Inimigos in hits:
+        I = Enemy(enemy_img)
+        all_sprites.add(I)
+        all_enemy.add(I)
+
     # Verifica se houve colisão entre nave e meteoro
     hits = pygame.sprite.spritecollide(player, all_enemy, True)
+    if len(hits) > 0:
+        # TOCA O SOM DA COLISÃO
+        boom_sound.play()
+        time.sleep(1)
+
+        game = False
+
     
-    #condição para o game para caso haja colisão
-    # if len(hits) > 0:
-    #     game = False
+    
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
@@ -212,3 +231,4 @@ while game:
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+ 
